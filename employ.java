17c0b376c8e40624f33bb40bfe0f29a6f4e9a935
udhayakumar v n <104062640+umeshudhaya@users.noreply.github.com>
2023-03-25 12:17:36 +0530

    Add files via upload

---
+++ employ.java
@@ -0,0 +1,16 @@
+class employ
+{
+   public static void main(String[] args){
+	   
+   System.out.println("udhayakumar");
+   String st ="udhayakumar1123 ";
+   char[] charArray = st.toCharArray();
+  for(char a : charArray)
+    System.out.print(a);
+   
+   }
+}
+   
+ 
+   
+     
